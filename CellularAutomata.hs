45cc03dc100dd50ce9b446e07d7ff465faec18f8
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-09-11 22:54:26 +0200

    define evolveOnce using map lookup

--- CellularAutomata.hs
+++ CellularAutomata.hs
@@ -3,7 +3,6 @@ module CellularAutomata where
 import qualified Data.List.Split as List.Split
 import qualified Data.Map as Map
 
-
 -- +-----------------------------------------------------------------+
 -- | Neighborhood    | 111 | 110 | 101 | 100 | 011 | 010 | 001 | 000 |
 -- +-----------------------------------------------------------------+
@@ -22,3 +21,7 @@ rule90 = Map.fromList [
 
 extractNeighborhoods :: [Int] -> [[Int]]
 extractNeighborhoods xs = List.Split.divvy 3 1 (0 : xs ++ [0])
+
+evolveOnce :: Map.Map [Int] Int -> [Int] -> [Int]
+evolveOnce rule state = map applyRuleToNeighborhood $ extractNeighborhoods state
+  where applyRuleToNeighborhood x = case Map.lookup x rule of Just y -> y
