5c0851703e8eb95e55fccfcf3ed2990d4895f29f
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-09-11 22:22:08 +0200

    define rule90 as a map

--- CellularAutomata.hs
+++ CellularAutomata.hs
@@ -1,6 +1,24 @@
 module CellularAutomata where
 
 import qualified Data.List.Split as List.Split
+import qualified Data.Map as Map
+
+
+-- +-----------------------------------------------------------------+
+-- | Neighborhood    | 111 | 110 | 101 | 100 | 011 | 010 | 001 | 000 |
+-- +-----------------------------------------------------------------+
+-- | New Center Cell |  0  |  1  |  0  |  1  |  1  |  0  |  1  |  0  |
+-- +-----------------------------------------------------------------+
+rule90 :: Map.Map [Int] Int
+rule90 = Map.fromList [
+  ([1,1,1], 0),
+  ([1,1,0], 1),
+  ([1,0,1], 0),
+  ([1,0,0], 1),
+  ([0,1,1], 1),
+  ([0,1,0], 0),
+  ([0,0,1], 1),
+  ([0,0,0], 0)]
 
 extractNeighborhoods :: [Int] -> [[Int]]
 extractNeighborhoods xs = List.Split.divvy 3 1 (0 : xs ++ [0])
